79cbf76f9c85bb0da7eeabca9d0d669383596367
Megan Christine McClure <mcclme02@luther.edu>
2020-03-13 14:47:53 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,44 @@
 class Dog 
-  def name=(dogs_name) #"setter method", takes in an argument of a dog's name and sets that argument equal to a variable
-    @this_dogs_name = dogs_name 
+
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end 
 
-  def name #"getter method", responsible for reading or reporting the name  
-    @this_dogs_name
-  end
+  def name 
+    @this_dogs_name 
+  end 
 
 end 
 
 lassie = Dog.new 
-lassie.name = "Lassie"
 
+lassie.name = "Lassie"
 puts lassie.name 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog 
+#   def name=(dogs_name) #"setter method", takes in an argument of a dog's name and sets that argument equal to a variable
+#     @this_dogs_name = dogs_name 
+#   end 
+
+#   def name #"getter method", responsible for reading or reporting the name  
+#     @this_dogs_name
+#   end
+
+# end 
+
+# lassie = Dog.new 
+# lassie.name = "Lassie"
+
+# puts lassie.name 
